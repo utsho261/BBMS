7f6656ac2b137dc09763b73e17e06d9411cae4b7
Utsho Roy <128884452+utsho261@users.noreply.github.com>
2024-10-16 09:14:10 +0600

    Add files via upload

---
+++ database.py
@@ -0,0 +1,10 @@
+
+import mysql.connector
+
+def connect_to_database():
+    return mysql.connector.connect(
+        host="localhost", 
+        user="root", 
+        password="", 
+        database="bloodbank"
+)
